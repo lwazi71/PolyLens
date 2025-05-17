15d87e0e21683b6ed3872185c9720b68f0f4ff2d
Sungmin Cha <css64534@gmail.com>
2025-05-17 10:56:26 -0700

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignTranslateApp from './components/SignTranslateApp';
+
+function App() {
+  return (
+    <div>
+      <SignTranslateApp />
+    </div>
+  );
+}
+
+export default App;
