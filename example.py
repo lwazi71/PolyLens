7a48841741529fb671a668b8bd7821a3a4d08691
Sungmin Cha <css64534@gmail.com>
2025-05-17 10:33:24 -0700

    Added image processing

--- example.py
+++ example.py
@@ -10,38 +10,38 @@ cap = cv2.VideoCapture(0)
 
 with mp_hands.Hands(
         max_num_hands=2,              # number of hands to detect
-        min_detection_confidence=0.5, # 손 인식 신뢰도
+        min_detection_confidence=0.5, # minimum confidence for detection
         min_tracking_confidence=0.5) as hands:
     
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
-            print("카메라 오류")
+            print("Camera error")
             break
         
-        # 좌우 반전 + RGB 변환
+        # Reverse left and right + RGB conversion
         frame = cv2.flip(frame, 1)
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
         
-        # 손 감지
+        # Detect hands
         results = hands.process(image)
         
-        # 다시 BGR로 전환
+        # Convert back to BGR
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        # 손 감지되었으면 랜드마크 시각화
+        # Vsualize landmarks
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     image,
                     hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,  # 손가락 뼈대 연결선
-                    mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),  # 점
-                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2))  # 선
+                    mp_hands.HAND_CONNECTIONS,  # finger connections
+                    mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),  # Dots
+                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2))  # Lines
                 
-        # 결과 출력
+        # Print hand landmarks
         cv2.imshow('Hand Trackor', image)
         
         if cv2.waitKey(5) & 0xFF == 27:
