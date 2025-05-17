dc00ad6cd6d2ff39a7478e8006ea69738cd89936
Khobaib Zafar <Kzafar@infoblox.com>
2025-05-17 11:16:52 -0700

    Back-end Code

--- cslt_translator/backend/models.py
+++ cslt_translator/backend/models.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel, Field
+from typing import List
+
 # Pydantic schemas for request/response
+
+class LandmarksRequest(BaseModel):
+    landmarks: List[List[float]] = Field(..., min_items=21, max_items=21)
+
+class PredictionResponse(BaseModel):
+    label: str
+    confidence: float
