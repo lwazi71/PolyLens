6b0d0605b78568f4ca9335185c71643735c3bc82
Sungmin Cha <css64534@gmail.com>
2025-05-17 15:55:07 -0700

    sm

--- cslt_translator/frontend/src/components/InfoSection.jsx
+++ cslt_translator/frontend/src/components/InfoSection.jsx
@@ -22,6 +22,8 @@ const InfoSection = () => {
       <div className={`color-transition ${!isGradientVisible ? 'hidden' : ''}`} />
       <div className="info-sections">
         <CommunicateSection />
+      </div>
+      <div className="trans-sections">
         <TranslationSection />
       </div>
     </>
