e8ff7e01cf8c8a360fcfbb49747f37139da26025
Sungmin Cha <css64534@gmail.com>
2025-05-17 21:20:03 -0700

    conflict

--- cslt_translator/frontend/src/components/sections/TranslationSection.jsx
+++ cslt_translator/frontend/src/components/sections/TranslationSection.jsx
@@ -1,11 +1,19 @@
+import './TranslationSection.css';
+
 const TranslationSection = () => {
   return (
-    <section className="info-section animate-section">
-      <div className="section-content">
-        <h2>Real-time Translation</h2>
-        <p>Our advanced AI model detects and translates sign language gestures instantly.</p>
+    <section className="translation-section animate-section">
+      <div className="blur-background"></div>
+      <div className="section-content-wrapper">
+        <div className="section-content">
+          <h2>Real-time Translation</h2>
+          <p>Our advanced AI model detects and translates sign language gestures instantly.</p>
+        </div>
+        <img
+          src="https://sloanreview.mit.edu/wp-content/uploads/2020/04/GEN-Latinovic-AI-Communication-1290x860-1.jpg"
+          alt="Translation demonstration"
+        />
       </div>
-      <img src="https://www.uploadvr.com/content/images/size/w1200/2021/04/QuestHandTrackingGreyscale.png" alt="Translation demonstration" />
     </section>
   );
 };
